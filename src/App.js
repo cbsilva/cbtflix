cc1b7cbd8ee0db8bfd96c1e454179a11b85e42ec
Cleberson <Silva>
2020-07-28 20:21:45 -0300

    corrigindo imports de bibliotecas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain/index';
-import Carrocel from './components/Carousel/index'; 
+import BannerMain from './components/BannerMain';
+import Carrocel from './components/Carousel'; 
 import Footer from './components/Footer';
 
+import dadosIniciais from './data/dados_iniciais.json';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <BannerMain 
           videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
           url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript parte da rotina das desenvolvedoras e desnvovlvedores. Mas o que els fazem afinal? Descubra com a Vanessa!"}>
+          videoDescription={"O leal comandante veterano da marinha A.K. Waters (Bruce Willis) é enviado para uma região da África completamente devastada pela guerra em uma missão super arriscada para resgatar a Dra. Lena Hendricks (Monica Belucci), uma cidadã americana que administra uma Missão"}>
       </BannerMain>
 
       <Carrocel
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png';
+import Logo from '../../assets/img/Logo2.png';
 import './Menu.css';
 
 function Menu () {
