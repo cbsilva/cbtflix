6e951d10ebee2a48379180ee144ab63e565197f7
Cleberson <Silva>
2020-07-28 06:43:36 -0300

    Primeiro versionamento do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain/index';
+import Carrocel from './components/Carousel/index'; 
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: '#141414'}}>      
+      <Menu />
+      
+      <BannerMain 
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript parte da rotina das desenvolvedoras e desnvovlvedores. Mas o que els fazem afinal? Descubra com a Vanessa!"}>
+      </BannerMain>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}>
+      </Carrocel>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}>
+      </Carrocel>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}>
+      </Carrocel>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}>
+      </Carrocel>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}>
+      </Carrocel>
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}>
+      </Carrocel>
+      
+      <Footer />
     </div>
   );
 }
