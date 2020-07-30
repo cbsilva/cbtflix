96c64ce70175f56c23592400c5db82f4190d3347
Cleberson <Silva>
2020-07-30 09:02:07 -0300

    componente de categoria
    criado componente de categoria e ajuste de rodas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
 import './index.css';
 import Home from './pages/Home';
 import CadastroVideo  from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
  // declaracao de variaveis
  const Pagina404 = () => (<div> Página 404</div>);
 
  /**
-  * 
   * quando usado exact ele procura somente a rota exata
   */
 
@@ -20,6 +19,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={Home}  exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
