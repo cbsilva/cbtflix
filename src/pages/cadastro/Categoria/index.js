839d4086376bb25696d68bc5dd45385b7a8651c1
Cleberson <Silva>
2020-07-31 09:04:07 -0300

    melhorando layout form categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,54 +1,122 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 import Button from '../../../components/Button';
 
+function useFormik({
+  initialValues
+
+}) {
+  const [values, setValues] = useState(initialValues);
+
+  function handleChange(event) {
+    const fieldName = event.target.getAttribute('name');
+    const { value } = event.target;
+
+    setValues({
+      ...values,
+      [fieldName]: value,
+    });
+  }  
+  console.log(values);
+  return {
+    values,
+    handleChange
+  }
+
+}
+
+
 function CadastroCategoria() {
+
+    const formik = useFormik({
+      initialValues: {
+        categoryName: '',
+        categoryColor: '',
+        categoryDescription: '',
+        categoryUser: '',
+      },
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+      },
+    });
     
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          console.log(formik.values);
+          alert('Olha o console');
+        }}>
+        <h1>Nova Categoria</h1>
+          <div className="formField">
+            <label htmlFor="categoryForm">
+              Nome da Categoria:
+            </label>
+            <input
+              type="text"
+              placeholder="Nome da Categoria"
+              name="categoryName"
+              id="categoryName"
+              onChange={formik.handleChange}
+              value={formik.values.categoryName}
+            />   
+          </div>  
 
-        <div>
-          <form> 
-            <div>
-              <label>
-                Nome da Categoria
-                <input type="text" name="nome" className="" />
-              </label>
-            </div>
-            <div>
-              <label>
-                Descrição
-                <input type="text" name="descricao" className="" />
-              </label> 
-            </div>
-            <div>
-              <label>
-                <label>
-                  Cor
-                  <input type="text" name="cor" className="" />
-                </label>
-              </label> 
-            </div> 
-            <div>
-              <label>
-                <label>
-                  Código de Segurança
-                  <input type="text" name="codigo_seguranca" className="" />
-                </label>
-              </label> 
-            </div>            
+          <div className="formField">
+            <label htmlFor="categoryForm">
+              Cor:
+            </label>
+            <input
+              type="text"
+              placeholder="Cor da Categoria"
+              name="categoryColor"
+              id="categoryColor"
+              onChange={formik.handleChange}
+              value={formik.values.categoryColor}
+            />   
+          </div>  
 
-            <Button variant="contained" color="primary">
-              Cadastrar
-            </Button>
+          <div className="formField">
+            <label htmlFor="categoryForm">
+              Cor:
+            </label>
+            <input
+              type="text"
+              placeholder="Descrição"
+              name="categoryDescription"
+              id="categoryDescription"
+              onChange={formik.handleChange}
+              value={formik.values.categoryDescription}
+            />   
+          </div>  
 
-            <Button variant="contained" color="info">
-              Limpar
-            </Button>
-          </form>
-        </div>
+          <div className="formField">
+            <label htmlFor="categoryForm">
+              Cor:
+            </label>
+            <input
+              type="text"
+              placeholder="Usuário"
+              name="categoryUser"
+              id="categoryUser"
+              onChange={formik.handleChange}
+              value={formik.values.categoryUser}
+            />   
+          </div>  
+          
+
+
+          <button type="submit">
+            Cadastrar
+          </button>  
+
+                     
+
+          
+          
+        </form>
+        
 
         <Link to="/">
           Ir para home
@@ -58,3 +126,20 @@ function CadastroCategoria() {
 }
 
 export default CadastroCategoria;
+
+
+/**
+ * <Button variant="contained" color="primary" type="submit">
+            Cadastrar
+          </Button>
+
+          <Button variant="contained" color="info">
+            Limpar
+          </Button>
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
