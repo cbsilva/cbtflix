5e7f62c15ede71d36900566cc57b4be3740bd313
Cleberson <Silva>
2020-07-31 06:49:43 -0300

    melhorias no componente de carrocel e criado form de catergoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
+    
     return (
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
+
+        <div>
+          <form> 
+            <div>
+              <label>
+                Nome da Categoria
+                <input type="text" name="nome" className="" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Descrição
+                <input type="text" name="descricao" className="" />
+              </label> 
+            </div>
+            <div>
+              <label>
+                <label>
+                  Cor
+                  <input type="text" name="cor" className="" />
+                </label>
+              </label> 
+            </div> 
+            <div>
+              <label>
+                <label>
+                  Código de Segurança
+                  <input type="text" name="codigo_seguranca" className="" />
+                </label>
+              </label> 
+            </div>            
+
+            <Button variant="contained" color="primary">
+              Cadastrar
+            </Button>
+
+            <Button variant="contained" color="info">
+              Limpar
+            </Button>
+          </form>
+        </div>
 
         <Link to="/">
           Ir para home
