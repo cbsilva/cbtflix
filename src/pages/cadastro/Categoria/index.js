a7055f5080b341d843dcb8a5dd235e65ea045b82
Cleberson <Silva>
2020-08-03 08:57:17 -0300

    criado editorconfig

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
