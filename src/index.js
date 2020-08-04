c382a408342ecce6f0642d0219bdfb0ef16f5b9c
Cleberson <Silva>
2020-08-04 06:49:10 -0300

    aplicando formatacao com eslint

--- src/index.js
+++ src/index.js
@@ -4,25 +4,24 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './index.css';
 import Home from './pages/Home';
-import CadastroVideo  from './pages/cadastro/Video';
+import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
- // declaracao de variaveis
- const Pagina404 = () => (<div> Página 404</div>);
+// declaracao de variaveis
+const Pagina404 = () => (<div> Página 404</div>);
 
- /**
+/**
   * quando usado exact ele procura somente a rota exata
   */
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home}  exact />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,52 @@
 import React from 'react';
 import Menu from '../../components/Menu';
 import BannerMain from '../../components/BannerMain';
-import Carrocel from '../../components/Carousel'; 
+import Carrocel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 
 import dadosIniciais from '../../data/dados_iniciais.json';
 
 function Home() {
   return (
-    <div style={{background: '#141414'}}>      
+    <div style={{ background: '#141414' }}>
       <Menu />
-      
-      <BannerMain 
-          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-          url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription={"O leal comandante veterano da marinha A.K. Waters (Bruce Willis) é enviado para uma região da África completamente devastada pela guerra em uma missão super arriscada para resgatar a Dra. Lena Hendricks (Monica Belucci), uma cidadã americana que administra uma Missão"}>
-      </BannerMain>
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription="O leal comandante veterano da marinha A.K. Waters (Bruce Willis) é enviado para uma região da África completamente devastada pela guerra em uma missão super arriscada para resgatar a Dra. Lena Hendricks (Monica Belucci), uma cidadã americana que administra uma Missão"
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}>
-      </Carrocel>
+        category={dadosIniciais.categorias[0]}
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[1]}>
-      </Carrocel>
+        category={dadosIniciais.categorias[1]}
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[2]}>
-      </Carrocel>
+        category={dadosIniciais.categorias[2]}
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[3]}>
-      </Carrocel>
+        category={dadosIniciais.categorias[3]}
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[4]}>
-      </Carrocel>
+        category={dadosIniciais.categorias[4]}
+      />
 
       <Carrocel
         ignoreFirstVideo
-        category={dadosIniciais.categorias[5]}>
-      </Carrocel>
-      
+        category={dadosIniciais.categorias[5]}
+      />
+
       <Footer />
     </div>
   );
